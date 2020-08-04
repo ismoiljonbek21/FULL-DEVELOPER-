4ff1953c90a7563928b7132c84f42a1e1ee96213
ISMOIL <69149237+ismoiljonbek21@users.noreply.github.com>
2020-08-04 11:56:25 +0500

    first commit is website

---
+++ home/js/script.js
@@ -0,0 +1,59 @@
+
+let chegirma = document.getElementsByClassName('chegirma')[0];
+let chegr = document.getElementById('chegr');
+chegr.onmousemove = function () {
+    chegirma.style.zIndex = '1';
+    chegirma.style.left = '700px';
+}
+chegr.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+
+}
+chegirma.onmousemove = function () {
+    chegirma.style.zIndex = '1';
+}
+chegirma.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+}
+
+let chegr2 = document.getElementById('chegr2');
+chegr2.onmousemove = function () {
+    chegirma.style.left = '750px';
+    chegirma.style.zIndex = '1';
+}
+chegr2.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+   
+
+}
+let chegr3 = document.getElementById('chegr3');
+chegr3.onmousemove = function () {
+    chegirma.style.left = '785px';
+    chegirma.style.zIndex = '1';
+}
+chegr3.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+
+
+}
+let chegr4 = document.getElementById('chegr4');
+chegr4.onmousemove = function () {
+    chegirma.style.left = '820px';
+    chegirma.style.zIndex = '1';
+}
+chegr4.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+
+
+}
+let chegr5 = document.getElementById('chegr5');
+chegr5.onmousemove = function () {
+    chegirma.style.left = '850px';
+    chegirma.style.zIndex = '1';
+}
+chegr5.onmouseout = function () {
+    chegirma.style.zIndex = '0';
+
+
+}
+
---
+++ sign in/js/script.js
@@ -0,0 +1,58 @@
+
+let chegirma = document.getElementsByClassName('chegirma')[0];
+let chegr = document.getElementById('chegr');
+chegr.onmousemove = function () {
+    chegirma.style.display = 'block';
+    chegirma.style.left = '700px';
+}
+chegr.onmouseout = function () {
+    chegirma.style.display = 'none';
+
+}
+chegirma.onmousemove = function () {
+    chegirma.style.display = 'block';
+}
+chegirma.onmouseout = function () {
+    chegirma.style.display = 'none';
+}
+
+let chegr2 = document.getElementById('chegr2');
+chegr2.onmousemove = function () {
+    chegirma.style.left = '750px';
+    chegirma.style.display = 'block';
+}
+chegr2.onmouseout = function () {
+    chegirma.style.display = 'none';
+   
+
+}
+let chegr3 = document.getElementById('chegr3');
+chegr3.onmousemove = function () {
+    chegirma.style.left = '785px';
+    chegirma.style.display = 'block';
+}
+chegr3.onmouseout = function () {
+    chegirma.style.display = 'none';
+
+
+}
+let chegr4 = document.getElementById('chegr4');
+chegr4.onmousemove = function () {
+    chegirma.style.left = '820px';
+    chegirma.style.display = 'block';
+}
+chegr4.onmouseout = function () {
+    chegirma.style.display = 'none';
+
+
+}
+let chegr5 = document.getElementById('chegr5');
+chegr5.onmousemove = function () {
+    chegirma.style.left = '850px';
+    chegirma.style.display = 'block';
+}
+chegr5.onmouseout = function () {
+    chegirma.style.display = 'none';
+
+
+}
